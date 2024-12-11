64148a01d184c88a48bfb4c14343bdbe047f6073
joatanfontoura <38365088+joatanfontoura@users.noreply.github.com>
2024-12-10 22:57:06 -0300

    Update calcAge.js

--- calcAge.js
+++ calcAge.js
@@ -6,3 +6,16 @@ function calcAge(byear){
 }
 
 calcAge(1991);
+
+////////////////////////////////////////
+// with return
+//
+
+function calcAge(byear){
+	var gd = new GlideDate();
+	var year = gd.getYear();
+	var age = year - byear;
+	return age; 
+}
+
+gs.info(calcAge(1991));
