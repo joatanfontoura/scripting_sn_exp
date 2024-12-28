017fcece9631297e39d2107bc1cb89803956ed05
joatanfontoura <38365088+joatanfontoura@users.noreply.github.com>
2024-12-28 16:22:01 -0300

    Update Funcao_Parametro_x_Argumento.js

--- Funcao_Parametro_x_Argumento.js
+++ Funcao_Parametro_x_Argumento.js
@@ -2,5 +2,5 @@ function soma(a, b) { // "a" e "b" são os parâmetros.
   return a + b;
 }
 
-const resultado = soma(5, 3); // "5" e "3" são os argumentos.
-console.log(resultado); // Saída: 8
+var resultado = soma(5, 3); // "5" e "3" são os argumentos.
+gs.log(resultado); // Saída: 8
