4ac27969894cc010ad1fb865e88cc7bd310052bf
joatanfontoura <38365088+joatanfontoura@users.noreply.github.com>
2025-01-24 16:44:33 -0300

    Create SumTwoNumbers.js

---
+++ SumTwoNumbers.js
@@ -0,0 +1,11 @@
+// Script Include
+
+function SumTwoNumbers (num1, num2) {
+	sum = num1 + num2;
+	return sum;
+}
+
+// Scripts - Background / Business Rule
+
+var result = SumTwoNumbers(8, 3);
+gs.info(result);
